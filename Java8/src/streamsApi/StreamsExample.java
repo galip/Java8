2d95710bc8bf0f3087dcbc9256c345a92b287e02
galip <galippala@gmail.com>
2023-11-04 15:45:10 +0300

    filter, peek.

---
+++ Java8/src/streamsApi/StreamsExample.java
@@ -0,0 +1,29 @@
+package streamsApi;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import data.Student;
+import data.StudentDatabase;
+
+public class StreamsExample {
+
+	public static void main(String args[]) {
+
+		Predicate<Student> studentGpaPredicate = (student -> student.getGrade() >= 3);
+
+		Map<String, List<String>> studentMap = StudentDatabase.getAllStudents().stream()
+				.peek((student -> {
+					System.out.println(student);
+				}))
+				.filter(studentGpaPredicate)
+				.peek((student -> {
+					System.out.println("Filtered: " + student);
+				}))
+				.collect(Collectors.toMap(Student::getName, Student::getFavouriteLectures));
+
+		System.out.println(studentMap);
+	}
+}
